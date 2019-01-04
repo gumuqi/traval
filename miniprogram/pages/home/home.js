e81ef89d9c8139897df5d646831e0e0fe8d5267b
hzfanyang <hzfanyang@corp.netease.com>
2019-01-04 14:26:00 +0800

    发布文章模块

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -2,25 +2,34 @@ Page({
   data: {
     articleList: []
   },
+  onPullDownRefresh() {
+    this.fetchData();
+  },
   /**
    * 获取最新发布的游记文章
    */
   fetchData: function () {
+    wx.showLoading({
+      title: '正在加载',
+    })
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
     db.collection('article').get({
       success: res => {
+        res.data.map(item => {
+          item.article_content = JSON.parse(item.article_content);
+        })
         this.setData({
           articleList: res.data
         })
-        console.log('[数据库] [查询记录] 成功: ', res)
+        wx.hideLoading();
       },
       fail: err => {
+        wx.hideLoading();
         wx.showToast({
           icon: 'none',
           title: '查询记录失败'
         })
-        console.error('[数据库] [查询记录] 失败：', err)
       }
     })
   },
